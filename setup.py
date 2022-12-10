4924e324cad5a9551e0e29292ac8d432b5bbb69d
judirispah <judi.rispah.123@gmail.com>
2022-12-11 00:14:41 +0530

    up

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+def get_requirements():
+    
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='judi',
+    author_email='judi.rispah.123@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
